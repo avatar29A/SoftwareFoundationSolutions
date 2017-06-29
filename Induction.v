a04e4c0c753e81e61f7a19d33e25907547364629
Boris Glebov <b.glebov@rambler-co.ru>
2017-06-30 00:32:11 +0300

    prove plus_swap without induction

--- Induction.v
+++ Induction.v
@@ -46,3 +46,52 @@ Proof.
   simpl. reflexivity.
   simpl. rewrite -> IHn'. reflexivity.
 Qed.
+
+Fixpoint double (n:nat) :=
+  match n with
+  | O => O
+  | S n' => S (S (double n'))
+  end.
+
+Lemma double_plus : forall n, double n = n + n .
+Proof.
+   intros n. induction n as [|n' IHn'].
+   reflexivity.
+   simpl.
+   rewrite -> IHn'.
+   rewrite <- plus_n_Sm.  
+   reflexivity.
+Qed.
+
+(* Proofs Within Proofs *)
+
+Theorem mult_0_plus' : forall n m : nat,
+  (0 + n) * m = n * m.
+Proof.
+  intros n m.
+  assert (H: 0 + n = n). { reflexivity. }
+  rewrite -> H.
+  reflexivity. 
+Qed.
+
+Theorem plus_rearrange_firsttry : forall n m p q : nat,
+  (n + m) + (p + q) = (m + n) + (p + q).
+Proof.
+  intros n m p q.
+  assert (H: (n + m) = (m + n)). { rewrite <- plus_comm. reflexivity. }
+  rewrite <- H.
+  reflexivity.
+Qed.
+
+Theorem plus_swap : forall n m p : nat,
+  n + (m + p) = m + (n + p).
+Proof.
+  intros n m p.
+  assert (H1: n + (m + p) = (n + m) + p). { rewrite <- plus_assoc. reflexivity. }
+  rewrite -> H1.
+  assert (H2: m + (n + p) = (m + n) + p). { rewrite <- plus_assoc. reflexivity. }
+  rewrite -> H2.
+  assert (H3: m + n = n + m). { rewrite <- plus_comm. reflexivity. }
+  rewrite <- H3. reflexivity.
+Qed.
+
